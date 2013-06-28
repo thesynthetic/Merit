82e103cb8a2791d248c66e450f0bbb7be2ec9ae5
Ryan <ryeguy_24@yahoo.com>
2013-06-27 22:51:00 -0400

    Updates

--- Merit/models.py
+++ Merit/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib import admin
-#from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 #User Model Extension
 
@@ -30,9 +30,12 @@ class MeritType(models.Model):
 	description = models.CharField(max_length=100)
 
 class Message(models.Model):
-	from_user = models.ForeignKey('User')
-	to_user = models.ForeignKey('User')
+	from_user = models.ForeignKey(User,related_name='message_from_user')
+	to_user = models.ForeignKey(User,related_name='message_to_user')
 
-
-
-admin.site.register(School, Class, Badge, Merit, MeritType, Message)
+admin.site.register(School)
+admin.site.register(Class)
+admin.site.register(Badge)
+admin.site.register(Merit)
+admin.site.register(MeritType)
+admin.site.register(Message)
